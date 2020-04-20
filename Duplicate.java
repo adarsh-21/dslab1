d5a00dfdd5d0b67f0433f8652bd03f94cbf5ecd9
Adarsh Chauhan <55660223+adarsh-21@users.noreply.github.com>
2020-04-20 17:40:17 +0530

    Add files via upload
    to find duplicate numbers in an array

---
+++ Duplicate.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Duplicate {
+    static void DuplicateNumbers(int arr[], int n) {
+        System.out.println("Duplicate elements in given array: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j])
+                    System.out.println(arr[j]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int n = S.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i <arr.length ; i++) {
+         arr[i] = S.nextInt();
+        }
+        DuplicateNumbers(arr, n);
+
+    }
+}
