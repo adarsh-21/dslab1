8be88dcafd954cca9fd3dea818e23c48d1e1f2a3
Adarsh Chauhan <55660223+adarsh-21@users.noreply.github.com>
2020-04-20 17:38:53 +0530

    Add files via upload

---
+++ occurances.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class occurances
+{
+    static void countOccurrences(int arr[], int n, int x)
+    {
+        int res = 0;
+        int i;
+        for (i=0; i<n; i++)
+            if (x == arr[i])
+                System.out.println(i);
+    }
+
+    public static void main(String args[])
+    {   Scanner S = new Scanner(System.in);
+        int n = S.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = S.nextInt();
+        }
+        System.out.println("Enter the element you want to find");
+        int x = S.nextInt();
+        countOccurrences(arr, n, x);
+    }
+} 
